d571baf479398b6184ec44461cee83aea007db50
UsefElbedwehy <elbedwehyy@gmail.com>
2024-01-02 19:46:15 +0200

    adding "SetIRQPriority" function

--- 02-MCAL/04-NVIC/Src/NVIC_prog.c
+++ 02-MCAL/04-NVIC/Src/NVIC_prog.c
@@ -13,7 +13,7 @@
 @brief enable interrupts
 @param[in] IRQNum
 @retval Local_u8ErrorState
-*/
+ */
 uint8_t NVIC_EnableIRQ(IRQn_t Copy_IRQn)
 {
 	uint8_t Local_u8ErrorState=OK;
@@ -21,17 +21,17 @@ uint8_t NVIC_EnableIRQ(IRQn_t Copy_IRQn)
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*Enable interrupt*/
-			(NVIC->NVIC_ISER[RegNum]) = (1<<BitNum);
+		(NVIC->NVIC_ISER[RegNum]) = (1<<BitNum);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
 	return Local_u8ErrorState;
 }
 
@@ -40,7 +40,7 @@ uint8_t NVIC_EnableIRQ(IRQn_t Copy_IRQn)
 @brief 
 @param[in] IRQNum
 @retval Local_u8ErrorState
-*/
+ */
 uint8_t NVIC_DisableIRQ(IRQn_t Copy_IRQn)
 {
 	uint8_t Local_u8ErrorState=OK;
@@ -48,17 +48,17 @@ uint8_t NVIC_DisableIRQ(IRQn_t Copy_IRQn)
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*Disable interrupt*/
-			(NVIC->NVIC_ICER[RegNum]) = (1<<BitNum);
+		(NVIC->NVIC_ICER[RegNum]) = (1<<BitNum);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
 	return Local_u8ErrorState;
 }
 /*
@@ -66,7 +66,7 @@ uint8_t NVIC_DisableIRQ(IRQn_t Copy_IRQn)
 @brief 
 @param[in] IRQNum
 @retval Local_u8ErrorState
-*/
+ */
 uint8_t NVIC_SetPendingFlag(IRQn_t Copy_IRQn)
 {
 	uint8_t Local_u8ErrorState=OK;
@@ -74,17 +74,17 @@ uint8_t NVIC_SetPendingFlag(IRQn_t Copy_IRQn)
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*changes interrupt state to pending*/
-			(NVIC->NVIC_ISPR[RegNum]) = (1<<BitNum);
+		(NVIC->NVIC_ISPR[RegNum]) = (1<<BitNum);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
 	return Local_u8ErrorState;
 }
 /*
@@ -92,7 +92,7 @@ uint8_t NVIC_SetPendingFlag(IRQn_t Copy_IRQn)
 @brief 
 @param[in] IRQNum
 @retval Local_u8ErrorState
-*/
+ */
 uint8_t NVIC_ClearPendingFlag(IRQn_t Copy_IRQn)
 {
 	uint8_t Local_u8ErrorState=OK;
@@ -100,17 +100,17 @@ uint8_t NVIC_ClearPendingFlag(IRQn_t Copy_IRQn)
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*removes pending state an interrupt*/
-			(NVIC->NVIC_ICPR[RegNum]) = (1<<BitNum);
+		(NVIC->NVIC_ICPR[RegNum]) = (1<<BitNum);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
 	return Local_u8ErrorState;
 }
 /*
@@ -118,7 +118,7 @@ uint8_t NVIC_ClearPendingFlag(IRQn_t Copy_IRQn)
 @brief 
 @param[in] IRQNum
 @retval Local_u8ErrorState ,*Copy_u8PtrActiveFlag
-*/
+ */
 uint8_t NVIC_GetPendingFlag(IRQn_t Copy_IRQn ,uint8_t* Copy_u8PtrActiveFlag)
 {
 	uint8_t Local_u8ErrorState=OK;
@@ -126,17 +126,17 @@ uint8_t NVIC_GetPendingFlag(IRQn_t Copy_IRQn ,uint8_t* Copy_u8PtrActiveFlag)
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*Get pending flag value*/
-			*Copy_u8PtrActiveFlag =(((NVIC->NVIC_ISPR[RegNum])>>BitNum) & 0x01);
+		*Copy_u8PtrActiveFlag =(((NVIC->NVIC_ISPR[RegNum])>>BitNum) & 0x01);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
 	return Local_u8ErrorState;
 }
 /*
@@ -144,24 +144,45 @@ uint8_t NVIC_GetPendingFlag(IRQn_t Copy_IRQn ,uint8_t* Copy_u8PtrActiveFlag)
 @brief 
 @param[in] IRQNum
 @retval Local_u8ErrorState , *Copy_u8PtrActiveFlag
-*/
+ */
 uint8_t NVIC_GetActiveFlag(IRQn_t Copy_IRQn ,uint8_t* Copy_u8PtrActiveFlag)
 {
-		uint8_t Local_u8ErrorState=OK;
+	uint8_t Local_u8ErrorState=OK;
 	/*calculate Register number*/
 	uint8_t RegNum= ( Copy_IRQn / 32);
 	/*calculate Bit number*/
 	uint8_t BitNum= ( Copy_IRQn % 32);
-	
+
 	if(Copy_IRQn<=FMPI2C1_ERROR)
 	{
 		/*Get pending flag value*/
-			Copy_u8PtrActiveFlag=(((NVIC->NVIC_IABR[RegNum])>>BitNum) & 0x01);
+		*Copy_u8PtrActiveFlag=(((NVIC->NVIC_IABR[RegNum])>>BitNum) & 0x01);
 	}
 	else
 	{
 		Local_u8ErrorState=NOK;
 	}
-	
+
+	return Local_u8ErrorState;
+}
+/*
+@fn SCB_SetPriorityGroup
+@brief set the priority group
+@param[in] Copy_IRQn ,Copy_priority
+@retval Local_u8ErrorState
+ */
+uint8_t	NVIC_SetIRQPriority(IRQn_t Copy_IRQn,uint8_t Copy_priority)
+{
+	uint8_t Local_u8ErrorState=OK;
+	if(Copy_IRQn <= FMPI2C1_ERROR)
+	{
+		NVIC->NVIC_IPR[Copy_IRQn] &= ~(0xF0);
+		NVIC->NVIC_IPR[Copy_IRQn] = (Copy_priority<<4);
+	}
+	else
+	{
+		Local_u8ErrorState=NOK;
+	}
+
 	return Local_u8ErrorState;
 }
