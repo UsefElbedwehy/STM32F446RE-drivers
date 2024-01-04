ee45031bd107e7e35dab9cfd64e2022df6855353
UsefElbedwehy <elbedwehyy@gmail.com>
2024-01-04 17:30:43 +0200

    SCB update

--- 02-MCAL/05-SCB/Inc/SCB_interface.h
+++ 02-MCAL/05-SCB/Inc/SCB_interface.h
@@ -13,14 +13,133 @@ typedef enum{
 #define VECTKEY_BIT   16
 #define PRIOGROUP_BIT 8
 
+/*@SHCSR_Reg_t*/
+typedef enum{
+	MEMFAULTACT=0,
+	BUSFAULTACT,
+	USGFAULTACT=3,
+	SVCALLACT=7,
+	MONITORACT,
+	PENDSVACT=10,
+	SYSTICKACT,
+	USGFAULTPENDED,
+	MEMFAULTPENDED,
+	BUSFAULTPENDED,
+	SVCALLPENDED,
+	MEMFAULTENA,
+	BUSFAULTENA,
+	USGFAULTENA
+}SHCSR_Reg_t;
+/*@MemManageF_Reg_t*/
+typedef enum{
+	IACCVIOL=0,
+	DACCVIOL,
+	MUNSTKERR=3,
+	MSTKERR,
+	MLSPERR,
+	MMARVALID=7,
+}MemManageF_Reg_t;
+/*@BusF_Reg_t*/
+typedef enum{
+	IBUSERR=0,
+	PRECISERR,
+	IMPRECISERR,
+	UNSTKERR,
+	STKERR,
+	LSPERR,
+	BFARVALID=7
+}BusF_Reg_t;
+/*@UsageF_Reg_t*/
+typedef enum{
+	UNDEFINSTR=0,
+	INVSTATE,
+	INVPC,
+	NOCP,
+	UNALIGNED=8,
+	DIVBYZERO=9
+}UsageF_Reg_t;
+
 /*
 @fn SCB_SetPriorityGroup
 @brief set the priority group
 @param[in] Copy_PRIOGroup
 @retval Local_u8ErrorState
- */
+*/
 uint8_t SCB_SetPriorityGroup(PRIOGroup_t Copy_PRIOGroup);
 
+/*
+@fn SCB_u8EnableMemMangeFaultEx
+@brief Enable Memory Management Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8EnableMemMangeFaultEx(void);
+/*
+@fn SCB_u8DisableMemMangeFaultEx
+@brief Disable Memory Management Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8DisableMemMangeFaultEx(void);
 
+/*
+@fn SCB_u8EnableBusFaultEx
+@brief Enable Bus Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8EnableBusFaultEx(void);
+/*
+@fn SCB_u8DisableBusFaultEx
+@brief Disable Bus Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8DisableBusFaultEx(void);
+
+/*
+@fn SCB_u8EnableUsageFaultEx
+@brief Enable Usage Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8EnableUsageFaultEx(void);
+/*
+@fn SCB_u8DisableUsageFaultEx
+@brief Disable Usage Fault Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8DisableUsageFaultEx(void);
+
+/*
+@fn SCB_u8EnableDivByZero
+@brief Enable the Division by zero Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8EnableDivByZero(void);
+/*
+@fn SCB_u8DisableUsageFaultEx
+@brief Disable the Division by zero Exception
+@param[in] void
+@retval void
+*/
+void SCB_u8DisableDivByZero(void);
+
+/*
+@fn SCB_u8SetSysHandlerCtrlSts
+@brief Set system handler control and status registers
+@param[in] Copy_SHCSR_Reg (type: @SHCSR_Reg_t)
+@retval Local_u8ErrorState
+*/
+uint8_t SCB_u8SetSysHandlerCtrlSts(SHCSR_Reg_t Copy_SHCSR_Reg);
+/*
+@fn SCB_u8ClrSysHandlerCtrlSts
+@brief Clear system handler control and status registers
+@param[in] Copy_SHCSR_Reg (type: @SHCSR_Reg_t)
+@retval Local_u8ErrorState
+*/
+uint8_t SCB_u8ClrSysHandlerCtrlSts(SHCSR_Reg_t Copy_SHCSR_Reg);
 
 #endif
--- 02-MCAL/05-SCB/Src/SCB_prog.c
+++ 02-MCAL/05-SCB/Src/SCB_prog.c
@@ -28,3 +28,132 @@ uint8_t SCB_SetPriorityGroup(PRIOGroup_t Copy_PRIOGroup)
 
 	return Local_u8ErrorState;
 }
+
+
+/*
+@fn SCB_u8EnableMemMangeFaultEx
+@brief Enable Memory Management Fault Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8EnableMemMangeFaultEx(void)
+{
+	SCB->SCB_SHCRS |= (1<<MEMFAULTENA);
+}
+/*
+@fn SCB_u8DisableMemMangeFaultEx
+@brief Disable Memory Management Fault Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8DisableMemMangeFaultEx(void)
+{
+	SCB->SCB_SHCRS &=~ (1<<MEMFAULTENA);
+}
+
+/*
+@fn SCB_u8EnableBusFaultEx
+@brief Enable Bus Fault Exception
+@param[in] void
+@retval Local_u8voidErrorState
+ */
+void SCB_u8EnableBusFaultEx(void)
+{
+	SCB->SCB_SHCRS |= (1<<BUSFAULTENA);
+}
+/*
+@fn SCB_u8DisableBusFaultEx
+@brief Disable Bus Fault Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8DisableBusFaultEx(void)
+{
+	SCB->SCB_SHCRS &=~ (1<<BUSFAULTENA);
+}
+
+/*
+@fn SCB_u8EnableUsageFaultEx
+@brief Enable Usage Fault Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8EnableUsageFaultEx(void)
+{
+	SCB->SCB_SHCRS |= (1<<USGFAULTENA);
+}
+/*
+@fn SCB_u8DisableUsageFaultEx
+@brief Disable Usage Fault Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8DisableUsageFaultEx(void)
+{
+	SCB->SCB_SHCRS &=~ (1<<USGFAULTENA);
+}
+
+/*
+@fn SCB_u8EnableDivByZero
+@brief Enable the Division by zero Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8EnableDivByZero(void)
+{
+	SCB->SCB_UFSR |= (1 << DIVBYZERO);
+}
+/*
+@fn SCB_u8DisableUsageFaultEx
+@brief Disable the Division by zero Exception
+@param[in] void
+@retval void
+ */
+void SCB_u8DisableDivByZero(void)
+{
+	SCB->SCB_UFSR &=~ (1 << DIVBYZERO);
+}
+
+/*
+@fn SCB_u8SetSysHandlerCtrlSts
+@brief Set system handler control and status registers
+@param[in] Copy_SHCSR_Reg (type: @SHCSR_Reg_t)
+@retval Local_u8ErrorState
+ */
+uint8_t SCB_u8SetSysHandlerCtrlSts(SHCSR_Reg_t Copy_SHCSR_Reg)
+{
+	uint8_t Local_u8ErrorState=OK;
+
+	if(( Copy_SHCSR_Reg >= MEMFAULTACT ) && (Copy_SHCSR_Reg <= USGFAULTENA))
+	{
+		SCB->SCB_SHCRS |= (1 << Copy_SHCSR_Reg);
+	}
+	else
+	{
+		Local_u8ErrorState=NOK;
+	}
+
+	return Local_u8ErrorState;
+}
+/*
+@fn SCB_u8ClrSysHandlerCtrlSts
+@brief Clear system handler control and status registers
+@param[in] Copy_SHCSR_Reg (type: @SHCSR_Reg_t)
+@retval Local_u8ErrorState
+ */
+uint8_t SCB_u8ClrSysHandlerCtrlSts(SHCSR_Reg_t Copy_SHCSR_Reg)
+{
+	uint8_t Local_u8ErrorState=OK;
+
+	if(( Copy_SHCSR_Reg >= MEMFAULTACT ) && (Copy_SHCSR_Reg <= USGFAULTENA))
+	{
+		SCB->SCB_SHCRS &=~ (1 << Copy_SHCSR_Reg);
+	}
+	else
+	{
+		Local_u8ErrorState=NOK;
+	}
+
+	return Local_u8ErrorState;
+}
+
